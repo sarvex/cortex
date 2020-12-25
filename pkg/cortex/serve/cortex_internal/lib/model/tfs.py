652924bb306121babeb278f0662b98da04d2b17f
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2020-12-25 03:10:11 +0200

    Increase TFS payload max length to 128 MB

--- pkg/cortex/serve/cortex_internal/lib/model/tfs.py
+++ pkg/cortex/serve/cortex_internal/lib/model/tfs.py
@@ -113,7 +113,8 @@ class TensorFlowServingAPI:
             {}
         )  # maps the model ID to the model metadata (signature def, signature key and so on)
 
-        self.channel = grpc.insecure_channel(self.address)
+        options = [('grpc.max_message_length', 128 * 1024 * 1024)]
+        self.channel = grpc.insecure_channel(self.address, options=options)
         self._service = model_service_pb2_grpc.ModelServiceStub(self.channel)
         self._pred = prediction_service_pb2_grpc.PredictionServiceStub(self.channel)
 
